07669c12b96cea9deca120d6937a38013ace50de
tikedamikilab <tikeda@mikilab.doshisha.ac.jp>
2019-08-21 07:57:46 +0900

    rails v6 webpacker解決できず

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  get 'hello/world'
+  resources :users
   # root 'application#hellohello'
-  root 'application#goodbye'
+  # root 'application#goodbye'
+  root 'users#index'
 end
